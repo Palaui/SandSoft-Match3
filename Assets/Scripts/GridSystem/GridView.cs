16e72ec8cb0549d3770176ac61436cb1315a2664
Marc Palau <sucdepressec@gmail.com>
2023-02-18 22:06:57 +0100

    Release

--- Assets/Scripts/GridSystem/GridView.cs
+++ Assets/Scripts/GridSystem/GridView.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GridView : MonoBehaviour
 {
+    private const float TIME_PER_SPAWN = 0.01f;
+    private const float SPAWN_ANIM_TIME = 0.4f;
+
     [SerializeField] private ElementTypeDataScriptable elementTypeData;
     [SerializeField] private MeshRenderer cellPrefab;
 
@@ -12,9 +15,18 @@ public class GridView : MonoBehaviour
 
     public void Setup(int[,] gridData)
     {
+        StopAllCoroutines();
+
         foreach (MeshRenderer renderer in renderers)
             Destroy(renderer.gameObject);
         renderers.Clear();
+
+        StartCoroutine(SetupCoroutine(gridData));
+    }
+
+    private IEnumerator SetupCoroutine(int[,] gridData)
+    {
+        WaitForSeconds wait = new WaitForSeconds(TIME_PER_SPAWN);
 
         for (int i = 0; i < gridData.GetLength(0); i++)
         {
@@ -26,12 +38,24 @@ public class GridView : MonoBehaviour
                 MeshRenderer cell = Instantiate(cellPrefab, position, Quaternion.identity, transform);
                 cell.material.SetTexture("_MainTex", elementTypeData.GetTextureByID(elementType));
                 renderers.Add(cell);
+
+                StartCoroutine(AnimateCellCoroutine(cell.transform));
+
+                yield return wait;
             }
         }
     }
 
-    private IEnumerator SetupCoroutine()
+    private IEnumerator AnimateCellCoroutine(Transform tr)
     {
-        yield return null;
+        float currentTime = 0;
+        while (currentTime < SPAWN_ANIM_TIME)
+        {
+            float alpha = currentTime / SPAWN_ANIM_TIME;
+            tr.localScale = (1 - (1 - alpha) * (1 - alpha)) * Vector3.one;
+
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
